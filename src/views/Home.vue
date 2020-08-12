93ca63d2edecc4fb677b91c27a448986c75ca4ed
Negin <nuggetnegin@gmail.com>
2020-08-12 14:02:28 -0400

    grid creation for perfect match

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,28 @@
 <template>
-    <div>
-        <button 
+<!-- /*banana, cherry, grape, orange, watermelon, apple*/
+/* 4 x 4*/ -->
+    <div class="container mx-auto">
+        <h1>Perfect Match</h1>
+
+        <div class="tile-container grid grid-cols-4 gap-4 grid-rows-4">
+            <div class="tile tile1-1">1</div>
+            <div class="tile tile-1-2">2</div>
+            <div class="tile tile-1-3">3</div>
+            <div class="tile tile-1-4">4</div>
+            <div class="tile tile-2-1">5</div>
+            <div class="tile tile-2-2">6</div>
+            <div class="tile tile-2-3">7</div>
+            <div class="tile tile-2-4">8</div>
+            <div class="tile tile-3-1">9</div>
+            <div class="tile tile-3-2">10</div>
+            <div class="tile tile-3-3">11</div>
+            <div class="tile tile-3-4">12</div>
+            <div class="tile tile-4-1">13</div>
+            <div class="tile tile-4-2">14</div>
+            <div class="tile tile-4-3">15</div>
+            <div class="tile tile-4-4">16</div>
+        </div>
+        <!-- <button 
             class="text-white rounded bg-pink-400 px-6 py-4 font-sans font-semibold shadow-lg hover:bg-pink-600 active:outline-none focus:outline-none"
             @click="toggleModal = !toggleModal">Open Modal
         </button>
@@ -16,9 +38,21 @@
         </div>
         <div v-if="toggleModal"
             class="absolute z-40 inset-0 opacity-25 bg-black">
-        </div>
+        </div> -->
     </div>
 </template>
+
+<style scoped>
+    .tile-container{
+        width: 650px;
+        margin: 0 auto;
+    }
+    .tile{
+        border: 1px solid black;
+        width: 150px;
+        height: 150px;
+    }
+</style>
 
 <script>
 export default {
